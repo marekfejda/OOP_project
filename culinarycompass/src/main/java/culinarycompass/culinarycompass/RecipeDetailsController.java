8331daed36a30b267b7bab90adb02ce0738c2d84
Marek Fejda <marekfejda@gmail.com>
2024-04-24 14:35:03 +0200

    ui changes

--- culinarycompass/src/main/java/culinarycompass/culinarycompass/RecipeDetailsController.java
+++ culinarycompass/src/main/java/culinarycompass/culinarycompass/RecipeDetailsController.java
@@ -3,7 +3,7 @@ package culinarycompass.culinarycompass;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 
@@ -11,7 +11,7 @@ import java.util.Objects;
 
 public class RecipeDetailsController {
     @FXML
-    private VBox rootVBox;
+    private AnchorPane rootAnchorPane;
     @FXML
     private Label nameLabel;
     @FXML
@@ -32,52 +32,49 @@ public class RecipeDetailsController {
     private ImageView notVeganBadge;
 
 
-
-    private final int badge_size_px = 50;
     public void setRecipe(Recipe recipe) {
-        rootVBox.setPadding(new Insets(10, 10, 10, 10));
-        nameLabel.setText("Name: " + recipe.getName());
+        rootAnchorPane.setPadding(new Insets(10, 10, 10, 10));
+        nameLabel.setText(recipe.getName());
         ingredientsLabel.setText("Ingredients: " + String.join(", ", recipe.getIngredients()));
         timeEstimationLabel.setText("Time: " + recipe.getTimeEstimation());
         portionsLabel.setText("Portions: " + recipe.getPortions());
+
+        recipeTextLabel.setWrapText(true);
+        recipeTextLabel.setMaxWidth(600);
         recipeTextLabel.setText("Recipe: " + recipe.getRecipeText());
 
-        try {
-            glutenFreeBadge.setFitWidth(badge_size_px);
-            glutenFreeBadge.setPreserveRatio(true);
-            notGlutenFreeBadge.setFitWidth(badge_size_px);
-            notGlutenFreeBadge.setPreserveRatio(true);
-            veganBadge.setFitWidth(badge_size_px);
-            veganBadge.setPreserveRatio(true);
-            notVeganBadge.setFitWidth(badge_size_px);
-            notVeganBadge.setPreserveRatio(true);
+        configureBadges(recipe);
+    }
 
-            if (recipe.isGlutenFree()) {
-                Image glutenFreeImage = new Image(getClass().getResourceAsStream("/culinarycompass/culinarycompass/badges/gluten-free.png"));
-                glutenFreeBadge.setImage(glutenFreeImage);
-                glutenFreeBadge.setVisible(true);
-                notGlutenFreeBadge.setVisible(false);
-            } else {
-                Image notGlutenFreeImage = new Image(getClass().getResourceAsStream("/culinarycompass/culinarycompass/badges/gluten-free-NOT.png"));
-                notGlutenFreeBadge.setImage(notGlutenFreeImage);
-                notGlutenFreeBadge.setVisible(true);
-                glutenFreeBadge.setVisible(false);
-            }
-            if (recipe.isVegetarian()) {
-                Image veganImage = new Image(getClass().getResourceAsStream("/culinarycompass/culinarycompass/badges/vegan.png"));
-                veganBadge.setImage(veganImage);
-                veganBadge.setVisible(true);
-                notVeganBadge.setVisible(false);
-            } else {
-                Image notVeganImage = new Image(getClass().getResourceAsStream("/culinarycompass/culinarycompass/badges/vegan-NOT.png"));
-                notVeganBadge.setImage(notVeganImage);
-                notVeganBadge.setVisible(true);
-                veganBadge.setVisible(false);
-            }
-        } catch (Exception e) {
-            e.printStackTrace(); // This will print any errors loading the images
+    private void configureBadges(Recipe recipe) {
+        int badge_size_px = 50;
+        glutenFreeBadge.setFitWidth(badge_size_px);
+        glutenFreeBadge.setPreserveRatio(true);
+        notGlutenFreeBadge.setFitWidth(badge_size_px);
+        notGlutenFreeBadge.setPreserveRatio(true);
+        veganBadge.setFitWidth(badge_size_px);
+        veganBadge.setPreserveRatio(true);
+        notVeganBadge.setFitWidth(badge_size_px);
+        notVeganBadge.setPreserveRatio(true);
+
+        if (recipe.isGlutenFree()) {
+            glutenFreeBadge.setImage(new Image(getClass().getResourceAsStream("/culinarycompass/culinarycompass/badges/gluten-free.png")));
+            glutenFreeBadge.setVisible(true);
+            notGlutenFreeBadge.setVisible(false);
+        } else {
+            notGlutenFreeBadge.setImage(new Image(getClass().getResourceAsStream("/culinarycompass/culinarycompass/badges/gluten-free-NOT.png")));
+            notGlutenFreeBadge.setVisible(true);
+            glutenFreeBadge.setVisible(false);
         }
-
+        if (recipe.isVegetarian()) {
+            veganBadge.setImage(new Image(getClass().getResourceAsStream("/culinarycompass/culinarycompass/badges/vegan.png")));
+            veganBadge.setVisible(true);
+            notVeganBadge.setVisible(false);
+        } else {
+            notVeganBadge.setImage(new Image(getClass().getResourceAsStream("/culinarycompass/culinarycompass/badges/vegan-NOT.png")));
+            notVeganBadge.setVisible(true);
+            veganBadge.setVisible(false);
+        }
     }
 
     @FXML
